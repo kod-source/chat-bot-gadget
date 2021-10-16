9372753b2ecf1ef67a13242b9de74020bd2eba87
堀航大 <kodenginner4521@gmail.com>
2021-10-17 00:14:42 +0900

    リファクタリングをしました

---
+++ front/app/lib/api/repository/authRepository.ts
@@ -0,0 +1,74 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { User } from '../../interfaces';
+import { loggedInUrl, loginUrl, signUpUrl } from '../hostUrl/url';
+
+interface loggedUser {
+  logged_in: boolean;
+  user: User;
+}
+
+interface SignUpUser {
+  status: string;
+  user: User;
+}
+
+export const loggedRepository = async (): Promise<loggedUser> => {
+  const res: AxiosResponse<loggedUser> = await axios.get(loggedInUrl, {
+    withCredentials: true,
+  });
+  return res.data;
+};
+
+export const loginRepository = async (
+  email: string,
+  password: string
+): Promise<loggedUser> => {
+  const res: AxiosResponse<loggedUser> = await axios.post(
+    loginUrl,
+    {
+      user: { email: email, password: password },
+    },
+    {
+      withCredentials: true,
+    }
+  );
+  return res.data;
+};
+
+export const signUpRepository = async (
+  name: string,
+  email: string,
+  password: string,
+  passwordConfirmation: string,
+  avatar?: string
+): Promise<SignUpUser> => {
+  const regex = /^(?=.*[./-])[a-zA-Z0-9.?/-]{8,24}$/;
+  if (!name && !email && !password && !passwordConfirmation) {
+    throw new Error('全ての項目を入力してください');
+  }
+  if (!regex.test(password)) {
+    if (password.length <= 8 || password.length >= 24) {
+      throw new Error('パスワードは8文字以上24文字以下に設定してください');
+    }
+    throw new Error(
+      'パスワードは記号を少なくとも一つ含めてくさださい。\nもしくは許可されていない文字種があります。'
+    );
+  }
+  if (password !== passwordConfirmation) {
+    throw new Error('パスワードが一致しません。');
+  }
+  const res: AxiosResponse<SignUpUser> = await axios.post(
+    signUpUrl,
+    {
+      user: {
+        name: name,
+        avatar: avatar,
+        email: email,
+        password: password,
+        password_confirmation: passwordConfirmation,
+      },
+    },
+    { withCredentials: true }
+  );
+  return res.data;
+};
