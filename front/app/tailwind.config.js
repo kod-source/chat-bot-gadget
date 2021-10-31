ba98d7208cce17e47e1ffe6b1543cf5dc0f51244
堀航大 <kodenginner4521@gmail.com>
2021-10-31 21:06:58 +0900

    不要なコメントを削除

--- front/app/tailwind.config.js
+++ front/app/tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './lib/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    // height: {
-    //   '600px': '600px',
-    //   '150px': '150px',
-    // },
     extend: {},
   },
   variants: {
