ab29604f5b9929fbad3d8ddb155c4a4069187587
堀航大 <kodenginner4521@gmail.com>
2021-11-07 17:32:21 +0900

    ホームページのレスポンシブ化

--- front/app/pages/index.tsx
+++ front/app/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Avatar, Button } from '@mui/material';
 import { Header } from 'lib/components/Header';
 import type { NextPage } from 'next';
-import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
 import { useContext } from 'react';
 import Link from 'next/link';
@@ -16,8 +15,7 @@ import { CardMessage } from 'lib/components/CardMessage';
 import { Footer } from 'lib/components/Footer';
 
 const Home: NextPage = () => {
-  const router = useRouter();
-  const { user, isSignedIn } = useContext(AuthContext);
+  const { isSignedIn } = useContext(AuthContext);
 
   return (
     <div>
@@ -27,9 +25,9 @@ const Home: NextPage = () => {
       <div className='fixed top-0 w-full z-10'>
         <Header />
       </div>
-      <div className='mt-16 py-36 flex justify-center bg-gray-300'>
-        <div className='my-2 mr-14 text-center'>
-          <h1 className='font-mono text-2xl mb-5'>
+      <div className='mt-16 py-36 sm:flex sm:justify-center bg-gray-300'>
+        <div className='sm:my-2 sm:mr-14 mx-4 text-center'>
+          <h1 className='font-mono sm:text-2xl text-xl mb-5'>
             おすすめのガジェット商品を教えてくれる
             <br />
             <span className='font-bold text-3xl'>GadgetBot</span>
@@ -58,6 +56,7 @@ const Home: NextPage = () => {
           alt='gadget_image'
           src={gadgetImage.src}
           sx={{ width: 200, height: 200 }}
+          className='m-auto sm:m-0'
         />
       </div>
       <div className='py-12'>
@@ -69,11 +68,11 @@ const Home: NextPage = () => {
             あなたに最適な機種を教えてくれます。
           </h2>
         </div>
-        <div className='mb-16 flex justify-center'>
+        <div className='mb-16 lg:flex lg:justify-center'>
           <Link href='/iphone'>
             <Card
-              className='mx-4 cursor-pointer hover:opacity-70'
-              sx={{ maxWidth: 400 }}
+              className='lg:mx-4 sm:m-auto sm:my-10 m-8 lg:my-0 cursor-pointer hover:opacity-70'
+              sx={{ maxWidth: 500 }}
             >
               <CardMessage
                 image={iphoneImage}
@@ -84,20 +83,20 @@ const Home: NextPage = () => {
           </Link>
           <Link href='/ipad'>
             <Card
-              className='mx-4 cursor-pointer hover:opacity-70'
-              sx={{ maxWidth: 400 }}
+              className='lg:mx-4 sm:m-auto sm:my-10 m-8 lg:my-0 cursor-pointer hover:opacity-70'
+              sx={{ maxWidth: 500 }}
             >
               <CardMessage
                 image={ipadImage}
                 title='iPad'
-                text='こちらは初めてiPadの購入を検討している方向けになっております。整備品済み製品もこちらでは、対象となっているためもし選ばれたものがすぐに購入ができない可能性もあるのでご了承ください。'
+                text='こちらは初めてiPadの購入を検討している方向けになっております。整備品済み製品もこちらでは、対象となっているためすぐに購入ができない可能性もあるのでご了承ください。'
               />
             </Card>
           </Link>
           <Link href='/mac'>
             <Card
-              className='mx-4 cursor-pointer hover:opacity-70'
-              sx={{ maxWidth: 400 }}
+              className='lg:mx-4 sm:m-auto sm:my-10 m-8 lg:my-0 cursor-pointer hover:opacity-70'
+              sx={{ maxWidth: 500 }}
             >
               <CardMessage
                 image={macBookImage}
@@ -107,17 +106,19 @@ const Home: NextPage = () => {
             </Card>
           </Link>
         </div>
-        <Link href='/signup' passHref>
-          <div className='text-center'>
-            <Button
-              variant='contained'
-              className='p-1 h-12 w-1/4 bg-green-500 transition ease-in-out duration-300 hover:bg-green-400 hover:-translate-y-1 hover:scale-110'
-            >
-              <AppRegistrationSharpIcon />
-              無料ユーザー登録はこちら
-            </Button>
-          </div>
-        </Link>
+        {!isSignedIn && (
+          <Link href='/signup' passHref>
+            <div className='text-center'>
+              <Button
+                variant='contained'
+                className='p-1 h-12 lg:w-1/4 sm:w-2/6 w-4/6 bg-green-500 transition ease-in-out duration-300 hover:bg-green-400 hover:-translate-y-1 hover:scale-110'
+              >
+                <AppRegistrationSharpIcon />
+                無料ユーザー登録はこちら
+              </Button>
+            </div>
+          </Link>
+        )}
       </div>
       <Footer />
     </div>
