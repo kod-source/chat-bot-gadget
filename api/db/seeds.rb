f7e3ee9b63604e2be1d8ec017c02fc1a8b6b4852
堀航大 <kodenginner4521@gmail.com>
2021-11-27 14:54:36 +0900

    fix

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -15,7 +15,7 @@ User.create!(
   memo: "こちらはテストユーザーです。\n" + "こちらに自由にメモなどを残しておくことができます。\n\n" + "是非ご活用ください！",
 )
 
-Product.create(
+Product.create!(
   name: 'iPad Air4',
   memo: "こちらのiPadはカラフルな色があり、お好みなカラーがあれば良いですね。\n" + "現在のiPadAir4はiPadProとの差が特になく、万人におすすでできる商品となってます。\n\n" + "※こちらの価格はWiFiモデルの価格となっており、価格は税込みの価格です。",
   image: 'https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/ipad-air-select-202009?wid=886&hei=1070&fmt=jpeg&qlt=80&.v=1599066777000',
@@ -23,7 +23,7 @@ Product.create(
   highest_price: 82280,
   url: 'https://www.apple.com/jp_edu_1460/shop/buy-ipad/ipad-air'
 )
-Product.create(
+Product.create!(
   name: 'iPad Pro 11インチ（第1世代）[整備済製品]',
   memo: "2020年に発売したiPad Proになります。" + "\n少し古く感じる方もいるかもしれませんが、そんなことなく現役扱うことができるかと思います。" + "\nこちらは整備済製品となっているため、iPad Air4よりも安い価格で購入することができ、私のイチオシの商品になっております。" + "\n※整備済製品のため、在庫がない可能性があるためご了承ください。" +"\n\n※こちらの価格はWiFiモデルの価格となっており、価格は税込みの価格です。",
   image: 'https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/refurb-ipad-pro-11-wifi-spacegray-2019?wid=1144&hei=1144&fmt=jpeg&qlt=80&.v=1581985486323',
@@ -31,7 +31,7 @@ Product.create(
   highest_price: 110800,
   url: 'https://www.apple.com/jp/shop/refurbished/ipad/11インチipad-pro'
 )
-Product.create(
+Product.create!(
   name: 'iPad Pro 11インチ（第2世代）[整備済製品]',
   memo: "2021年に発売したiPad Proになります。少し前の機種になりますが、とても性能も高くどのユーザーでも満足できるのではないかと思います。" + "\n※整備済製品のため、在庫がない可能性があるためご了承ください。" + "\n\n※こちらの価格はWiFiモデルの価格となっており、価格は税込みの価格です。",
   image: 'https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/refurb-ipad-pro-11-wifi-spacegray-2020?wid=1144&hei=1144&fmt=jpeg&qlt=80&.v=1627667236000',
@@ -39,7 +39,7 @@ Product.create(
   highest_price: 121800,
   url: 'https://www.apple.com/jp_edu_1460/shop/buy-ipad/ipad-air'
 )
-Product.create(
+Product.create!(
   name: 'iPad Pro 11インチ（第3世代）',
   memo: "こちらは最新のiPad Proになります。マシンスペックがとても高くて、使いこなせるユーザーはなかなかいないのでないかと思います。" + "\nしかし、ロマンなスペックと最高級のディスプレイが搭載されているため、最高のタブレットになること間違い無いです！" + "\n\n※こちらの価格はWiFiモデルの価格となっており、価格は税込みの価格です。",
   image: 'https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/ipad-pro-11-select-202104_GEO_JP?wid=970&amp;hei=1020&amp;fmt=png-alpha&amp;.v=1617899794000',
@@ -47,7 +47,7 @@ Product.create(
   highest_price: 226800,
   url: 'https://www.apple.com/jp/shop/buy-ipad/ipad-pro'
 )
-Product.create(
+Product.create!(
   name: 'iPad Pro 11インチ（第1世代）[整備済製品]',
   memo: "2020年に発売したiPad Proになります。" + "\n少し古く感じる方もいるかもしれませんが、そんなことなく現役扱うことができるかと思います。" + "\nこちらは整備済製品となっているため、お求めやすい価格で購入することができ、私のイチオシの商品になっております。" + "\n※整備済製品のため、在庫がない可能性があるためご了承ください。" + "\n\n※こちらの価格はWiFiモデルの価格となっており、価格は税込みの価格です。",
   image: 'https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/refurb-ipad-pro-12-cell-spacegray-2019?wid=1144&hei=1144&fmt=jpeg&qlt=95&.v=1581985511549',
@@ -55,7 +55,7 @@ Product.create(
   highest_price: 124800,
   url: 'https://www.apple.com/jp/shop/refurbished/ipad/12-9インチipad-pro'
 )
-Product.create(
+Product.create!(
   name: 'iPad Pro 12.9インチ（第4世代）[整備済製品]',
   memo: "2021年に発売したiPad Proになります。少し前の機種になりますが、とても性能も高くどのユーザーでも満足できるのではないかと思います。" + "\n※整備済製品のため、在庫がない可能性があるためご了承ください。" + "\n\n※こちらの価格はWiFiモデルの価格となっており、価格は税込みの価格です。",
   image: 'https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/refurb-ipad-pro-12-wifi-spacegray-2020?wid=1144&hei=1144&fmt=jpeg&qlt=80&.v=1626721066000',
@@ -63,7 +63,7 @@ Product.create(
   highest_price: 138800,
   url: 'https://www.apple.com/jp/shop/refurbished/ipad/12-9インチipad-pro'
 )
-Product.create(
+Product.create!(
   name: 'iPad Pro 12.9インチ（第5世代）',
   memo: "こちらは最新のiPad Proになります。マシンスペックがとても高くて、使いこなせるユーザーはなかなかいないのでないかと思います。" + "\nしかし、ロマンなスペックと最高級のディスプレイが搭載されているため、最高のタブレットになること間違い無いです！" + "\n\n※こちらの価格はWiFiモデルの価格となっており、価格は税込みの価格です。",
   image: 'https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/ipad-pro-12-select-cell-spacegray-202104_GEO_JP?wid=940&hei=1112&fmt=p-jpg&qlt=95&.v=1617920528000',
@@ -71,7 +71,7 @@ Product.create(
   highest_price: 261800,
   url: 'https://www.apple.com/jp/shop/buy-ipad/ipad-pro'
 )
-Product.create(
+Product.create!(
   name: 'iPad（第9世代）',
   memo: "こちらは無印のiPadになります。最安のiPadになっており、エントリー機としては最高の商品になるのでは無いかと思います。" + "\nさらにiPad（第9世代）からは最低スペックの容量が32GBから64GBに増加し、実質的なプライスダウンになっているのでiPadを試したい人には最適だと思います。" + "\n\n※こちらの価格はWiFiモデルの価格となっており、価格は税込みの価格です。",
   image: 'https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/ipad-2021-hero-landing-wifi?wid=900&hei=1046&fmt=jpeg&qlt=80&.v=1630973863000',
@@ -79,7 +79,7 @@ Product.create(
   highest_price: 57800,
   url: 'https://www.apple.com/jp/shop/buy-ipad/ipad-10-2'
 )
-Product.create(
+Product.create!(
   name: 'iPad mini6',
   memo: "こちらはiPad mini6になります。今回のアップデートで大幅な進化遂げました。iPad Air4をそのまま小さくしたものをイメージしてもらえるとわかりやすいかと思います。" + "\n特に持ち運んで使用したり、家でゴロゴロしながら使うなどいろいろな用途が考えらるかと思います。" + "\niPad miniは昔からこのサイズ感が大好きでminiファンが沢山いるくらいサイズ感はとてもちょうど良く、重量軽くハマる人にはとことんハマる商品になっているかと思います。" + "\n\n※こちらの価格はWiFiモデルの価格となっており、価格は税込みの価格です。",
   image: 'https://store.storeimages.cdn-apple.com/8567/as-images.apple.com/is/ipad-mini-select-202109?wid=1080&hei=1060&fmt=jpeg&qlt=80&.v=1631751017000',
