0252b03e6c0cdd6a972a39dba807351193c39961
堀航大 <kodenginner4521@gmail.com>
2021-12-12 14:53:50 +0900

    ipad_date_set_create

---
+++ front/app/lib/components/Chats.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+export const Chats:FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- front/app/pages/ipad/index.tsx
+++ front/app/pages/ipad/index.tsx
@@ -1,11 +1,25 @@
 import { NextPage } from 'next';
 import React from 'react';
+import Head from 'next/head';
+import { Header } from 'lib/components/Header';
+import { Footer } from 'lib/components/Footer';
+import IpadDate from 'lib/DataSet/ipad_data.json';
 
 const Ipad: NextPage = () => {
+  console.log(IpadDate);
   return (
-    <div>
-      <h1>iPad</h1>
-    </div>
+    <>
+      <Head>
+        <title>iPad</title>
+      </Head>
+      <div className='fixed top-0 w-full z-10'>
+        <Header />
+      </div>
+      <div className='mt-28'>
+        <h1>ipad_page</h1>
+      </div>
+      <Footer />
+    </>
   );
 };
 
