27163b74b8e45fa967ccb091f27bf6c8b50e2004
堀航大 <kodenginner4521@gmail.com>
2021-12-12 16:08:48 +0900

    create_ipad_show_page

--- front/app/pages/ipad/index.tsx
+++ front/app/pages/ipad/index.tsx
@@ -1,12 +1,14 @@
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import { Header } from 'lib/components/Header';
 import { Footer } from 'lib/components/Footer';
 import IpadDate from 'lib/DataSet/ipad_data.json';
+import { Button } from '@mui/material';
+import Image from 'next/image';
 
 const Ipad: NextPage = () => {
-  console.log(IpadDate);
+  const [startIpadBot, setStartIpadBot] = useState(false);
   return (
     <>
       <Head>
@@ -15,9 +17,37 @@ const Ipad: NextPage = () => {
       <div className='fixed top-0 w-full z-10'>
         <Header />
       </div>
-      <div className='mt-28'>
-        <h1>ipad_page</h1>
-      </div>
+      {startIpadBot ? (
+        <div className='mt-40'>
+          <h1>診断スタート</h1>
+        </div>
+      ) : (
+        <div className='mt-40 text-center'>
+          <h1 className='text-4xl font-bold text-center text-gray-500'>
+            最適なiPadページへようこそ！
+          </h1>
+          <hr className='p-0 w-1/2 m-auto text-gray-500 mt-1 mb-3' />
+          <Image
+            src='/IpadPageImage.jpg'
+            width={500}
+            height={500}
+            className='pointer-events-none'
+          />
+          <h3 className='text-xl font-bold'>iPad</h3>
+          <p className='text-yellow-700 my-3'>
+            こちらはiPadを診断してくれるページになります
+          </p>
+          <p className='my-3'>早速スタートしますか？</p>
+          <div className='mt-3 mb-16'>
+            <Button
+              className='mx-2 my-1 sm:my-0 w-56 bg-yellow-600 text-white hover:bg-yellow-700 static p-4 transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110'
+              onClick={() => setStartIpadBot(true)}
+            >
+              [ はい ]
+            </Button>
+          </div>
+        </div>
+      )}
       <Footer />
     </>
   );
