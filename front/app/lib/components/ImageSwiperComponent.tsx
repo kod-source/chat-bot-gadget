11fdf54814530256ed49cb6ee47356cba52beb2d
堀航大 <kodenginner4521@gmail.com>
2021-12-30 15:36:02 +0900

    画像ホバー時にラベルの表示をするようにした

--- front/app/lib/components/ImageSwiperComponent.tsx
+++ front/app/lib/components/ImageSwiperComponent.tsx
@@ -3,12 +3,57 @@ import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
 import { ImageSwiper } from 'lib/api/Entity/ImageSwiper';
+import { ClickAwayListener } from '@mui/material';
+import { Box } from '@mui/system';
 
 SwiperCore.use([Navigation, Pagination]);
 
 interface Props {
   imageSwipers: ImageSwiper[];
 }
+
+const SwiperSlideComponent: FC<{
+  imageSwiper: ImageSwiper;
+  activeIndex: number;
+  index: number;
+  slideTo: (index: number) => void;
+}> = ({ imageSwiper, activeIndex, index, slideTo }) => {
+  const [hoverShowTitle, setHoverShowTitle] = useState(false);
+  return (
+    <ClickAwayListener onClickAway={() => setHoverShowTitle(false)}>
+      <Box>
+        {hoverShowTitle ? (
+          <>
+            <p className='absolute transition-opacity opacity-100 duration-1000 text-white text-xs border-gray-500 bg-gray-500 p-1 rounded'>
+              {imageSwiper.title}
+            </p>
+            <p className='opacity-0'>1</p>
+          </>
+        ) : (
+          <p className='opacity-0'>1</p>
+        )}
+        <div
+          className={
+            activeIndex === index + 1
+              ? 'border-b-2 border-gray-300 mx-2'
+              : 'hover:border-b-2 border-gray-200 border-opacity-70 mx-2'
+          }
+          onMouseEnter={() => setHoverShowTitle(true)}
+          onMouseLeave={() => setHoverShowTitle(false)}
+        >
+          <Image
+            key={imageSwiper.title}
+            src={imageSwiper.url}
+            width={70}
+            height={70}
+            onClick={() => slideTo(index + 1)}
+            className='cursor-pointer hover:opacity-70'
+          />
+        </div>
+      </Box>
+    </ClickAwayListener>
+  );
+};
 
 export const ImageSwiperComponent: FC<Props> = ({ imageSwipers }) => {
   const [swiperInstance, setSwiperInstance] = useState<SwiperCore | null>(null);
@@ -42,22 +87,13 @@ export const ImageSwiperComponent: FC<Props> = ({ imageSwipers }) => {
       </Swiper>
       <div className='mt-5 flex justify-start'>
         {imageSwipers.map((imageSwiper, index) => (
-          <div
-            className={
-              activeIndex === index + 1
-                ? 'border-b-2 border-gray-300 mx-2'
-                : 'hover:border-b-2 border-gray-200 border-opacity-70 mx-2'
-            }
-          >
-            <Image
-              key={imageSwiper.title}
-              src={imageSwiper.url}
-              width={70}
-              height={70}
-              onClick={() => slideTo(index + 1)}
-              className='cursor-pointer hover:opacity-70'
-            />
-          </div>
+          <SwiperSlideComponent
+            key={imageSwiper.title}
+            imageSwiper={imageSwiper}
+            activeIndex={activeIndex}
+            index={index}
+            slideTo={slideTo}
+          />
         ))}
       </div>
     </>
